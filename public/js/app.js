f6e6a6ab1291693458daa70b590600e1749e166f
Akash Jain <mr.akashj@gmail.com>
2016-04-03 20:05:09 -0400

    google analytics

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,3 @@
-angular.module('rappor-js', ['ngRoute', 'appRoutes', 'MainCtrl', 'RecordCtrl', 'SendReportCtrl', 'ExamineBrowserCtrl', 'GenerateCSVCtrl', 'AnalyzeCSVCtrl']);
+angular.module('rappor-js', ['ngRoute', 'appRoutes', 'MainCtrl', 'RecordCtrl', 'SendReportCtrl', 'ExamineBrowserCtrl', 'GenerateCSVCtrl', 'AnalyzeCSVCtrl', 'angulartics', 'angulartics.google.analytics']);
 
 console.log("app.js touched");
