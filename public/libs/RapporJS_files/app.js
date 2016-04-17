286f8d95f4aec14c1a606337ed3535050db3ff2d
Akash Jain <mr.akashj@gmail.com>
2016-04-16 23:09:04 -0400

    cleanup

--- public/libs/RapporJS_files/app.js
+++
@@ -1,3 +0,0 @@
-angular.module('rappor-js', ['ngRoute', 'appRoutes', 'MainCtrl', 'RecordCtrl', 'AboutCtrl', 'SendReportCtrl', 'ExamineBrowserCtrl', 'GenerateCSVCtrl', 'AnalyzeCSVCtrl', 'angulartics', 'angulartics.google.analytics']);
-
-console.log("app.js touched");
