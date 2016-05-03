7d1972be15efa043c69d877b9670b36d63a6bfa5
Akash Jain <mr.akashj@gmail.com>
2016-05-03 17:12:03 -0400

    default

--- fabfile.py
+++ fabfile.py
@@ -68,7 +68,7 @@ def runb():
 	assert_directory()
 	local("browserify --fast -t coffeeify rappor.coffee -o public/js/rappor-js/rappor.js")
 	local("browserify -t coffeeify rappor-examine.coffee -o public/js/rappor-js/rappor-examine.js")
-	local("browserify -t coffeeify rappor-csvs.coffee -o public/js/rappor-js/rappor-csvs.js")
+	# local("browserify -t coffeeify rappor-csvs.coffee -o public/js/rappor-js/rappor-csvs.js")
 	# local("coffee thesis-exp-simulation.coffee -o public/js/rappor-js/thesis-exp-simulation.js")
 
 	local("uglifyjs public/js/rappor-js/rappor.js -o public/js/rappor-js/rappor.min.js -m --stats --keep-fnames")
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,3 @@
-angular.module('rappor-js', ['ngRoute', 'appRoutes', 'MainCtrl', 'RecordCtrl', 'AboutCtrl', 'SendReportCtrl', 'ExamineBrowserCtrl', 'GenerateCSVCtrl', 'AnalyzeCSVCtrl', 'angulartics', 'angulartics.google.analytics']);
+angular.module('rappor-js', ['ngRoute', 'appRoutes', 'MainCtrl', 'RecordCtrl', 'AboutCtrl', 'SendReportCtrl', 'ExamineBrowserCtrl', 'GenerateCSVCtrl', 'AnalyzeCSVCtrl', 'angulartics.google.analytics']);
 
 console.log("app.js touched");
