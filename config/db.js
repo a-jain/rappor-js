d7f6bcab78af5db693569050ef74da19f95cb3af
Akash Jain <mr.akashj@gmail.com>
2016-05-20 22:37:55 -0400

    default

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 // config/db.js
 
 module.exports = {
-    // url : 'mongodb://akshjn:nokian95@ds035385.mlab.com:35385/rappor-test'
-    url: 'mongodb://akshjn:nokian95@ds021230-a0.mlab.com:21230,ds021230-a1.mlab.com:21230/rappor-js?replicaSet=rs-ds021230'
+    url : 'mongodb://akshjn:nokian95@ds035385.mlab.com:35385/rappor-test'
+    // url: 'mongodb://akshjn:nokian95@ds021230-a0.mlab.com:21230,ds021230-a1.mlab.com:21230/rappor-js?replicaSet=rs-ds021230'
 }
